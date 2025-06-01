319a2004f6ae87d523e9f16ffed372e15c03170a
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-06-01 17:59:51 +0500

    update

--- variables.js
+++ variables.js
@@ -27,3 +27,17 @@ console.log(str + 7)
 
 console.log(" the type of str is "+typeof(str))
 
+
+// Distionary 
+
+const dictionary = {
+  apple: "A fruit that keeps the doctor away",
+  book: "A set of written or printed pages",
+  code: "A system of words for communication",
+  light: "Natural agent that makes things visible",
+  dream: "A series of thoughts during sleep"
+};
+
+
+
+console.log(" the meaning of this word in dictionary is "+dis.code)
