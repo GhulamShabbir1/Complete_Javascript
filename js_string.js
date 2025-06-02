c3c21376ca7db5f0aac52483ecd24b2839d13963
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-06-02 11:08:39 +0500

    update

---
+++ js_string.js
@@ -0,0 +1,21 @@
+let name = "Ghulam Shabbir"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+let friend = 'hamza'
+console.log(friend)
+
+// let myfriend = "Shubh' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "raw"
+let boy2 = "rick"
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+
+let fruit = "Bana\"na"
+console.log(fruit)
