143abd11a9de3804dde4ab26c7ed7a8b9d08af78
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-06-02 11:16:40 +0500

    update

--- js_string.js
+++ js_string.js
@@ -19,3 +19,38 @@ console.log(sentence)
 
 let fruit = "Bana\"na"
 console.log(fruit)
+
+// String Methods
+
+console.log(name.length)
+
+console.log(name.toUpperCase())
+
+console.log(name.toLowerCase())
+
+console.log(name.slice(2, 4))
+
+console.log(name.slice(2))
+
+console.log(name.replace("Har", "Per"))
+
+console.log(name.concat(" is a friend of ", friend, " Ok"))
+
+let friend2 = "      Meena        "
+console.log(friend2)
+console.log(friend2.trim())
+
+let fr = "Roman" + "seth" + "dean"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr[4])
+
+
+console.log(fr)
+// QuickQuiz: Use a for loop to print a string
+
+for (let i=0 ;i<fr.length; i++){
+    console.log(fr[i])
+}
