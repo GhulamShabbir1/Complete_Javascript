5742900f88a581a12fd44f7ad8aaf2ea3481231c
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-06-02 12:47:48 +0500

    update

--- array_complete.js
+++ array_complete.js
@@ -82,3 +82,34 @@ let faltarray=arr11.flat(2)  // remove nested arrays and add its value in main a
 let arr12=[551,22,3,14,5,6,7,8,229]
 a=arr12.sort()
 console.log(a)
+
+
+let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+let num_even_more = [211, 212, 213, 214, 415, 416, 417, 418, 419]
+console.log(num.length)
+delete num[0]
+console.log(num.length)
+
+let newArray = num.concat(num_more, num_even_more)
+console.log(newArray)
+console.log(num, num_more)
+
+// sort method
+let compare = (a, b)=>{
+  return b - a
+}
+let num2 = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+num2.sort(compare)
+num2.reverse()
+console.log(num2)
+
+// Splice and Slice
+let num1 = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+let deletedValues = num.splice(2, 4, 1021, 1022, 1023, 1024, 1025) // at position 2 add 4 values
+console.log(num)
+console.log(deletedValues)
+
+let newNum3 = num.slice(3)
+let newNum4 = num1.slice(3, 5)
+console.log(newNum3)
+console.log(newNum4)
