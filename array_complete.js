e4d4b82d9bac9d770e8aaa4c84ee4a80a59453f4
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-06-02 15:58:53 +0500

    update

--- array_complete.js
+++ array_complete.js
@@ -134,3 +134,27 @@ for (let item of num3){
 for (let i in num3){
   console.log(" using for in loop "+num3[i])
 }
+
+
+let arr = [45, 23, 21]
+// Array map method
+let a = arr.map((value, index, array) => {
+  // console.log(value, index, array)
+  return value + index
+})
+// console.log(arr)
+
+// Array filter method
+let arry1 = [45, 23, 21, 0, 3, 5]
+let a2 = arr2.filter((a1) => {
+  return a1 < 10
+})
+// console.log(a2, arr2)
+
+// Array reduce method
+let arry2 = [1, 2, 3, 5, 2, 1]
+const reduce_func = (h1, h2) => {
+  return h1 + h2
+}
+let newarr3 = arr3.reduce(reduce_func)
+console.log(newarr3)
